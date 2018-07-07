b818e7e7fdcc05f84492a29c5aef8af15fdf945c
Velpula Narasimha <40896960+NarasimhaVelpula@users.noreply.github.com>
2018-07-07 06:52:00 +0530

    http://reslts.000webhostapp.com/
    check this link for any doubts

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+.back{
+    background-color:lightgrey;
+}
+
+
+</style>
+
+
+</head>
+<body class="back">
+<center style="padding-top:10%">
+
+<td> <a href="adminlogin.php">
+<img src="adminlogin.jpg" alt="Flowers in Chania"  >
+ </a>
+ <a href="stafflogins.php">
+<img src="staff.png" alt="Flowers in Chania"  >
+</a>
+<a href="studentlogin.php">
+<img src="studentlogin.png" alt="Flowers in Chania"  >
+</a>
+
+
+</body>
+</html>
